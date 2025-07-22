055dbf1b73945a97ea3b069fdd209c0a4ff8bbbf
FlyCloud_C <fly.cloud.blry@gmail.com>
2025-07-21 21:46:43 -0700

    Load variable from .env for backend

--- backend/blry_website/settings.py
+++ backend/blry_website/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(dotenv_path=BASE_DIR / '.env')
 
 # Application definition
 
